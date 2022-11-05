7ec41ee975f63f7878acb8198d2f41fb1dd416f5
Sousa-Edson <edson3711.es@gmail.com>
2022-11-05 19:38:59 -0300

    < modificado arquivo AdocaoanimaisApplication>

--- src/main/java/com/belval/adocaoanimais/AdocaoanimaisApplication.java
+++ src/main/java/com/belval/adocaoanimais/AdocaoanimaisApplication.java
@@ -2,12 +2,19 @@ package com.belval.adocaoanimais;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class AdocaoanimaisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdocaoanimaisApplication.class, args);
+	}
+	@GetMapping("/")
+	public String index() {
+		return "coloque na url  /pet";
 	}
 
 }
