c38c0aad6cffb7a1ff0f4911946342c3e46e9357
Sousa-Edson <edson3711.es@gmail.com>
2022-11-17 12:55:41 -0300

    <adicionando arquivos de cor pet>

---
+++ src/main/java/com/belval/adocaoanimais/controller/CorController.java
@@ -0,0 +1,53 @@
+package com.belval.adocaoanimais.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.belval.adocaoanimais.model.PetCor;
+import com.belval.adocaoanimais.repository.CorRepository;
+
+@Controller
+public class CorController {
+	@Autowired
+	private CorRepository repository;
+
+	@PostMapping("/pet/pet-cor")
+	public ModelAndView novo(PetCor cor) {
+		ModelAndView mv = new ModelAndView("redirect:../pet/pet-cor");
+		repository.save(cor);
+		return mv;
+	}
+
+	@GetMapping("/pet/pet-cor")
+	public String list(Model model) {
+		model.addAttribute("c", new PetCor());
+		model.addAttribute("cor", repository.findAll());
+		return "pet-cor";
+	}
+
+	@GetMapping("/pet/pet-cor/{id}")
+	public String detalhe(@PathVariable("id") int id, Model model) {
+		PetCor c = repository.findById(id);
+		if (c == null) {
+			return "cor-nao-encontrada";
+		}
+		model.addAttribute("c", c);
+		return "pet-cor";
+	}
+
+	@GetMapping("/pet/pet-cor/{id}/edit")
+	public String edit(@PathVariable("id") int id, Model model) {
+		PetCor c = repository.findById(id);
+		if (c == null) {
+			return "cor-nao-encontrada";
+		}
+		model.addAttribute("c", c);
+		return "pet-cor";
+
+	}
+}
---
+++ src/main/java/com/belval/adocaoanimais/model/PetCor.java
@@ -0,0 +1,52 @@
+
+package com.belval.adocaoanimais.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class PetCor {
+ @Id
+ @GeneratedValue(strategy=GenerationType.AUTO)
+private Integer id;
+	private String cor;
+	private boolean ativo;
+
+	public PetCor(Integer id, String cor, boolean ativo) {
+		super();
+		this.id = id;
+		this.cor = cor;
+		this.ativo = ativo;
+	}
+
+	public PetCor() {
+		super();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+}
