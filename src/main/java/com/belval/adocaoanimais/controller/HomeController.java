83082228331cd60d64f8e34dcf6be205eb70ff14
Sousa-Edson <edson3711.es@gmail.com>
2022-11-16 23:15:02 -0300

    inserção de arquivo home e controller

---
+++ src/main/java/com/belval/adocaoanimais/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.belval.adocaoanimais.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+
+	@GetMapping("/home")
+	public String home2() {
+		return "home";
+	}
+
+}
