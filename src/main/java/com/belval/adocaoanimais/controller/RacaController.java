cb6bbb740ffa97c4c24ed54fe1afc05fcf00c5e5
Sousa-Edson <edson3711.es@gmail.com>
2022-11-19 00:13:50 -0300

    incluido arquivos html

--- src/main/java/com/belval/adocaoanimais/controller/RacaController.java
+++ src/main/java/com/belval/adocaoanimais/controller/RacaController.java
@@ -9,78 +9,74 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.belval.adocaoanimais.model.PetCor;
 import com.belval.adocaoanimais.model.PetRaca;
-import com.belval.adocaoanimais.repository.CorRepository;
+import com.belval.adocaoanimais.repository.RacaRepository;
 
 @Controller
-public class CorController {
+public class RacaController {
 	@Autowired
-	private CorRepository repository;
+	private RacaRepository repository;
 
-	@PostMapping("/pet/pet-cor")
-	public ModelAndView novo(PetCor cor) {
-		ModelAndView mv = new ModelAndView("redirect:../pet/pet-cor");
-		if (!cor.getCor().isEmpty()) {
-			repository.save(cor);
+	@PostMapping("/pet/pet-raca")
+	public ModelAndView novo(PetRaca raca) {
+
+		ModelAndView mv = new ModelAndView("redirect:../pet/pet-raca");
+
+		if (!raca.getRaca().isEmpty()) {
+			repository.save(raca);
 		}
 
 		return mv;
 	}
 
-	@GetMapping("/pet/pet-cor")
+	@GetMapping("/pet/pet-raca")
 	public String list(Model model) {
-		model.addAttribute("c", new PetCor());
-		model.addAttribute("cor", repository.findAll());
-		return "pet-cor";
+
+		model.addAttribute("c", new PetRaca());
+		model.addAttribute("raca", repository.findAll());
+		return "pet-raca";
 	}
 
-	@GetMapping("/pet/pet-cor/{id}/edit")
+	@GetMapping("/pet/pet-raca/{id}/edit")
 	public String edit(@PathVariable("id") int id, Model model, ModelMap m) {
-		PetCor c = repository.findById(id);
+		PetRaca c = repository.findById(id);
 		if (c == null) {
 			return "cor-nao-encontrada";
 		} else {
-			model.addAttribute("cor", repository.findAll());
+			model.addAttribute("raca", repository.findAll());
 			model.addAttribute("c", c);
-			m.addAttribute("msg", "msg");
-			return "pet-cor";
+			m.addAttribute("msg", "mensagem");
+			return "pet-raca";
 		}
 
 	}
 
-//	@GetMapping("/pet/pet-cor/{id}/excluir")
-//	public String excluir(@PathVariable("id") int id, Model model) {
-//		repository.deleteById(id);
-//
-//		return list(model);
-//
-//	}
-
-	@GetMapping("/pet/pet-cor/{id}/excluir")
+	@GetMapping("/pet/pet-raca/{id}/excluir")
 	public String excluir(@PathVariable("id") int id, Model model, ModelMap m) {
 		// repository.deleteById(id);
-		PetCor e = repository.findById(id);
+		PetRaca e = repository.findById(id);
 		if (e == null) {
 			return list(model);
 		} else {
 			System.out.println(e.getId());
-			model.addAttribute("cor", repository.findAll());
-			model.addAttribute("c", new PetCor());
+			model.addAttribute("raca", repository.findAll());
+			model.addAttribute("c", new PetRaca());
 			model.addAttribute("e", e);
 			m.addAttribute("exc", true);
-			return "pet-cor";
+			return "pet-raca";
 		}
 	}
 
-	@GetMapping("/pet/pet-cor/{id}/excluindo")
+	@GetMapping("/pet/pet-raca/{id}/excluindo")
 	public String excluirConfirmado(@PathVariable("id") int id, Model model) {
-		PetCor e = repository.findById(id);
+		PetRaca e = repository.findById(id);
 		if (e == null) {
 			return list(model);
 		} else {
 			repository.deleteById(id);
 			return list(model);
 		}
+
 	}
+
 }
