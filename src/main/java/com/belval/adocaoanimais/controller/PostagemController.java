dd863a73cf0fd98b003871984f83f0c58c422deb
Sousa-Edson <edson3711.es@gmail.com>
2022-11-24 07:50:33 -0300

    tratanmento de imagem postagem finalizado

--- src/main/java/com/belval/adocaoanimais/controller/PostagemController.java
+++ src/main/java/com/belval/adocaoanimais/controller/PostagemController.java
@@ -1,5 +1,7 @@
 package com.belval.adocaoanimais.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -21,8 +24,8 @@ import com.belval.adocaoanimais.repository.PostagemRepository;
 public class PostagemController {
 	@Autowired
 	private PostagemRepository repository;
-//	@Autowired
-//	public static String caminhoImagens = "/home/edson/Dev/workspace/adocao-animais/src/main/resources/imagens/";
+	@Autowired
+	public static String caminhoImagens = "/home/edson/Dev/workspace/adocao-animais/src/main/resources/imagens/img-postagem";
 //	public static String caminhoImagens = "D:/Aulas/2022/LIP1/workspace/data";
 
 	@GetMapping("/postagem/postagem-lista")
@@ -39,20 +42,30 @@ public class PostagemController {
 	}
 
 	@PostMapping("/postagem/postagem-novo")
-	public ModelAndView novo(Postagem postagem, @RequestParam("file") MultipartFile arquivo) {
+	public ModelAndView novo(Postagem postagem, @RequestParam("file-img") MultipartFile arquivo) {
 		System.out.println("arquivo " + arquivo);
 		ModelAndView mv = new ModelAndView("redirect:../postagem/postagem-lista");
 		if (!postagem.getTitulo().isEmpty()) {
 			repository.save(postagem);
 		}
+		try {
+			System.out.println("entrando");
+			if (!arquivo.isEmpty()) {
+				System.out.println("arquivo  = " + arquivo);
+				byte[] bytes = arquivo.getBytes();
+				Path caminho = Paths
+						.get(caminhoImagens + String.valueOf(postagem.getId()) + arquivo.getOriginalFilename());
+				Files.write(caminho, bytes);
+				postagem.setCaminhoImagem(String.valueOf(postagem.getId()) + arquivo.getOriginalFilename());
+				repository.save(postagem);
+			}
+		} catch (Exception e) {
+			System.out.println("erro--> " + e);
+			e.printStackTrace();
+		}
 
 		return mv;
 	}
-
-//	@GetMapping("/postagem/postagem-detalhe")
-//	public String detalhe() {
-//		return "postagem/postagem-detalhe";
-//	}
 
 	@GetMapping("/postagem/postagem-detalhe/{id}")
 	public String detalhe(@PathVariable("id") int id, Model model) {
@@ -70,42 +83,14 @@ public class PostagemController {
 
 	}
 
-//	@PostMapping("/postagem/postagem-detalhe")
-//	public String detalhe2() {
-//		return "postagem/postagem-detalhe";
-//	}
-
-	/*
-	 * AQUI TESTE PARA SALVAR IMAGEM EM LOCAL -> MAS SEM SUCESSO SALVO EM PASTA MAS
-	 * NAÃO CONSIGO RETORNAR OU RETORNO DE RESOUCES MAS NAO CONSIGO SALVAR LA
-	 * 
-	 * @PostMapping("/postagem/postagem-novo") public ModelAndView novo(Postagem
-	 * postagem, @RequestParam("file") MultipartFile arquivo) {
-	 * System.out.println("arquivo " + arquivo); ModelAndView mv = new
-	 * ModelAndView("redirect:../postagem/postagem-lista"); if
-	 * (!postagem.getTitulo().isEmpty()) { repository.save(postagem); } try {
-	 * System.out.println("entrando"); if (!arquivo.isEmpty()) {
-	 * System.out.println("arquivo  = " + arquivo); byte[] bytes =
-	 * arquivo.getBytes(); Path caminho = Paths .get(caminhoImagens +
-	 * String.valueOf(postagem.getId()) + arquivo.getOriginalFilename());
-	 * Files.write(caminho, bytes);
-	 * postagem.setCaminhoImagem(String.valueOf(postagem.getId()) +
-	 * arquivo.getOriginalFilename());
-	 * System.out.println("img -- "+postagem.getCaminhoImagem());
-	 * repository.save(postagem); } } catch (Exception e) {
-	 * System.out.println("erro"); System.out.println(e); e.printStackTrace(); }
-	 * 
-	 * return mv; }
-	 * 
-	 * @GetMapping("/postagem/postagem-detalhe/{id}") public String
-	 * detalhe(@PathVariable("id") int id, Model model) { System.out.println("ola");
-	 * Postagem c = repository.findById(id);
-	 * System.out.println("titulo -- "+c.getTitulo()); if (c == null) { return
-	 * "nao-encontrada"; } else { System.out.println("pega"+c.getCaminhoImagem());
-	 * model.addAttribute("postagem", repository.findAll()); model.addAttribute("c",
-	 * c); model.addAttribute("i", caminhoImagens); return
-	 * "postagem/postagem-detalhe"; }
-	 * 
-	 * }
-	 */
+	@GetMapping("/pet/mostrarImagem/{imagem}")
+	@ResponseBody
+	public byte[] retornarImagem(@PathVariable("imagem") String imagem) throws IOException {
+		File imagemArquivo = new File(caminhoImagens + imagem);
+		if (imagem != null || imagem.trim().length() > 0) {
+			System.out.println("Imagem pasada --> "+imagemArquivo);
+			return Files.readAllBytes(imagemArquivo.toPath());
+		}
+		return null;
+	}
 }
