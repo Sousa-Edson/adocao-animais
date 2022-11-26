40df8707926ad5f15e58e3afc5beb6fca4b7b131
Sousa-Edson <edson3711.es@gmail.com>
2022-11-25 21:30:23 -0300

    implementando home -> mostrar postagens salva no banco

--- src/main/java/com/belval/adocaoanimais/controller/HomeController.java
+++ src/main/java/com/belval/adocaoanimais/controller/HomeController.java
@@ -1,27 +1,38 @@
 package com.belval.adocaoanimais.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.belval.adocaoanimais.model.Postagem;
+import com.belval.adocaoanimais.repository.PostagemRepository;
 
 @Controller
 public class HomeController {
+	@Autowired
+	private PostagemRepository repository;
+
+	@GetMapping("/pet/home")
+	public String list(Model model) {
+
+//		model.addAttribute("c", new Postagem());
+		model.addAttribute("p", repository.findAll());
+		return "home";
+	}
+
 	@GetMapping("/")
 	public String home() {
-		return index();
+		return "home";
 	}
 
 	@GetMapping("/home")
 	public String home2() {
-		return index();
+		return "home";
 	}
 
 	@GetMapping("/pet")
 	public String home3() {
-		return index();
-	}
-
-	@GetMapping("/pet/home")
-	public String index() {
 		return "home";
 	}
 
