34206b58853507883f31bab7f253b2faffa4a177
Sousa-Edson <edson3711.es@gmail.com>
2022-11-26 01:45:20 -0300

    usuario -> criando classe controller

---
+++ src/main/java/com/belval/adocaoanimais/controller/UsuarioController.java
@@ -0,0 +1,36 @@
+package com.belval.adocaoanimais.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.belval.adocaoanimais.model.Usuario;
+import com.belval.adocaoanimais.repository.UsuarioRepository;
+
+@Controller
+public class UsuarioController {
+	@Autowired
+	private UsuarioRepository repository;
+
+	@PostMapping("/pet/usuario/usuario-novo")
+	public ModelAndView novo(Usuario usuario) {
+		ModelAndView mv = new ModelAndView("redirect:../login");
+		if (!usuario.getNome().isEmpty()) {
+			repository.save(usuario);
+		}
+
+		return mv;
+	}
+
+	@GetMapping("/pet/usuario/usuario-novo")
+	public ModelAndView novo2(Usuario usuario) {
+		ModelAndView mv = new ModelAndView("redirect:../login");
+		if (!usuario.getNome().isEmpty()) {
+			repository.save(usuario);
+		}
+
+		return mv;
+	}
+}
