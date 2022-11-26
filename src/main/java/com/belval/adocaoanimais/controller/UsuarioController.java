80468655cd73119f8c0ecb1e5384cccf9a6a18c7
Sousa-Edson <edson3711.es@gmail.com>
2022-11-26 13:57:40 -0300

    usuario -> corrigindo perfil

--- src/main/java/com/belval/adocaoanimais/controller/UsuarioController.java
+++ src/main/java/com/belval/adocaoanimais/controller/UsuarioController.java
@@ -2,10 +2,12 @@ package com.belval.adocaoanimais.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.belval.adocaoanimais.model.PetCor;
 import com.belval.adocaoanimais.model.Usuario;
 import com.belval.adocaoanimais.repository.UsuarioRepository;
 
@@ -15,7 +17,7 @@ public class UsuarioController {
 	private UsuarioRepository repository;
 
 	@PostMapping("/pet/usuario/usuario-novo")
-	public ModelAndView novo(Usuario usuario) {
+	public ModelAndView registro(Usuario usuario) {
 		ModelAndView mv = new ModelAndView("redirect:../login/novo");
 		if (!usuario.getNome().isEmpty()) {
 			repository.save(usuario);
@@ -33,7 +35,12 @@ public class UsuarioController {
 
 	/* LOGIN-CADASTRO */
 	@GetMapping("/pet/login/novo")
-	public String cadastro() {
-		return "/pessoa/cadastroUsu";
+	public ModelAndView cadastro(Usuario usuario, Model model) {
+		ModelAndView mv = new ModelAndView("/pessoa/cadastroUsu");
+		 
+//			usuario.setNome("Tadeo");
+			model.addAttribute("u", usuario);
+		 
+		return mv;
 	}
 }
