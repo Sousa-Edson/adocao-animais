98beaea0d00eeebbc84fc1438e505ad1f5f89d29
Edson Sousa <edson3711.es@gmail.com>
2022-12-05 23:01:53 -0300

    criação de classe animal model

--- src/main/java/com/belval/adocaoanimais/model/Animal.java
+++ src/main/java/com/belval/adocaoanimais/model/Animal.java
@@ -1,17 +1,33 @@
 package com.belval.adocaoanimais.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Animal {
+	@Id
+	 @GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
 	private String nome;
-	private String raca;
-	private int idade;
+	private int raca;
+	private int cor;
+	private int porte;
+	private String especie;
+	private int sexo;
+	private int vacina;
 
-	public Animal(int id, String nome, String raca, int idade) {
+	public Animal(int id, String nome, int raca, int cor, int porte, String especie, int sexo, int vacina) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.raca = raca;
-		this.idade = idade;
+		this.cor = cor;
+		this.porte = porte;
+		this.especie = especie;
+		this.sexo = sexo;
+		this.vacina = vacina;
 	}
 
 	public Animal() {
@@ -34,20 +50,52 @@ public class Animal {
 		this.nome = nome;
 	}
 
-	public String getRaca() {
+	public int getRaca() {
 		return raca;
 	}
 
-	public void setRaca(String raca) {
+	public void setRaca(int raca) {
 		this.raca = raca;
 	}
 
-	public int getIdade() {
-		return idade;
+	public int getCor() {
+		return cor;
 	}
 
-	public void setIdade(int idade) {
-		this.idade = idade;
+	public void setCor(int cor) {
+		this.cor = cor;
+	}
+
+	public int getPorte() {
+		return porte;
+	}
+
+	public void setPorte(int porte) {
+		this.porte = porte;
+	}
+
+	public String getEspecie() {
+		return especie;
+	}
+
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+
+	public int getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(int sexo) {
+		this.sexo = sexo;
+	}
+
+	public int getVacina() {
+		return vacina;
+	}
+
+	public void setVacina(int vacina) {
+		this.vacina = vacina;
 	}
 
 }
