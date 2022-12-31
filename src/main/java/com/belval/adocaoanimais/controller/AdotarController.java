0fc140a28e68c91cde05e4d7d68b0ee78fa64057
Edson Sousa <edson3711.es@gmail.com>
2022-12-30 21:35:14 -0300

    update de projeto

--- src/main/java/com/belval/adocaoanimais/controller/AdotarController.java
+++ src/main/java/com/belval/adocaoanimais/controller/AdotarController.java
@@ -1,15 +1,38 @@
 package com.belval.adocaoanimais.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.belval.adocaoanimais.model.Animal;
+import com.belval.adocaoanimais.repository.AnimalRepository;
 
 @Controller
+@RequestMapping(value = "/pet/private/intencao-adotar")
 public class AdotarController {
+    @Autowired
+	private AnimalRepository animalRepository;
 
-    @GetMapping("/pet/intencao-adotar")
-    public String novo() {
-        return "/catalogo/intencao-adotar";
-    }
+  
+
+    @GetMapping("")
+	public ModelAndView index() {
+		List<Animal> animais = this.animalRepository.findAll();
+		ModelAndView mv = new ModelAndView("private/intencao/index");
+		mv.addObject("animais", animais);
+		return mv;
+	}
+    @GetMapping("/new")
+	public ModelAndView nnew() {
+		List<Animal> animais = this.animalRepository.findAll();
+		ModelAndView mv = new ModelAndView("private/intencao/new");
+		mv.addObject("animais", animais);
+		return mv;
+	}
 
     
     
---
+++ src/main/java/com/belval/adocaoanimais/controller/GaleriaController.java
@@ -0,0 +1,57 @@
+package com.belval.adocaoanimais.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.belval.adocaoanimais.model.Animal;
+import com.belval.adocaoanimais.model.PetCor;
+import com.belval.adocaoanimais.model.PetRaca;
+import com.belval.adocaoanimais.repository.AnimalRepository;
+import com.belval.adocaoanimais.repository.CorRepository;
+import com.belval.adocaoanimais.repository.RacaRepository;
+
+@Controller
+@RequestMapping(value = "/pet/public/galeria")
+public class GaleriaController {
+    @Autowired
+	private AnimalRepository animalRepository;
+    @Autowired
+	private RacaRepository racaRepository;
+	@Autowired
+	private CorRepository corRepository;
+
+    @GetMapping("")
+    public ModelAndView index() {
+        List<Animal> animais = this.animalRepository.findAll();
+        ModelAndView mv = new ModelAndView("public/galeria/index");
+        mv.addObject("animais", animais);
+        return mv;
+    }
+
+    @GetMapping("/{id}")
+	public ModelAndView show(@PathVariable Long id) {
+		System.out.println("**** ID: " + id);
+		Optional<Animal> optional = this.animalRepository.findById(id);
+		if (optional.isPresent()) {
+			Animal animal = optional.get();
+			ModelAndView mv = new ModelAndView("public/galeria/show");
+			Optional<PetRaca> racas = racaRepository.findById(optional.get().getRaca());
+			mv.addObject("listaRaca", racas.get());
+			Optional<PetCor> cores = corRepository.findById(optional.get().getCor());
+			mv.addObject("listaCor", cores.get());
+			mv.addObject(animal);
+			return mv;
+		} else {
+			System.out.println("$$$$$$$$$$$ Não achou animal");
+            return index();
+		}
+	}
+
+}
