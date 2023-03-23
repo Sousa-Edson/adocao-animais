a74e0f445dec7b9d0d4ede60d1023fc1f8590006
Sousa-Edson <edson3711.es@gmail.com>
2023-03-23 07:41:58 -0300

    criação inicial do dataloader ,para iniciar o banco ja com dados

---
+++ src/main/java/com/belval/adocaoanimais/DataLoader.java
@@ -0,0 +1,42 @@
+package com.belval.adocaoanimais;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.belval.adocaoanimais.enums.Permissao;
+import com.belval.adocaoanimais.model.Usuario;
+import com.belval.adocaoanimais.repository.UsuarioRepository;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+	LocalDateTime agora = LocalDateTime.now();
+	String s = "25/07/1981";
+	DateTimeFormatter parser = new DateTimeFormatterBuilder()
+			// dia/mês/ano
+			.appendPattern("dd/MM/uuuu")
+			// valor default para o horário (zero para meia-noite)
+			.parseDefaulting(ChronoField.HOUR_OF_DAY, 9).toFormatter();
+	LocalDateTime ontem = LocalDateTime.parse(s, parser);
+
+	@Autowired
+	UsuarioRepository usuarioRepository;
+
+	Permissao permissao;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		/** SALVAR USUARIO **/
+
+		usuarioRepository.save(new Usuario("Edson", "Sousa", "123.123.123", "10/01/2000", "masculino",
+				"edson@edson.com", "11 9999-3333", "123", permissao.ADMINISTRADOR, true));
+
+	}
+
+}
