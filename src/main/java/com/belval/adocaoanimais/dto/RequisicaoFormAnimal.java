7ec15b68e5056d17a7fd5e916c85a320a6c0e49b
Sousa-Edson <edson3711.es@gmail.com>
2023-03-25 00:55:39 -0300

    correção de animal para mostrar imagem mesmo se tiver vazia

--- src/main/java/com/belval/adocaoanimais/dto/RequisicaoFormAnimal.java
+++ src/main/java/com/belval/adocaoanimais/dto/RequisicaoFormAnimal.java
@@ -1,6 +1,7 @@
 package com.belval.adocaoanimais.dto;
+ 
 
-import java.sql.Date;
+import java.util.Date;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
