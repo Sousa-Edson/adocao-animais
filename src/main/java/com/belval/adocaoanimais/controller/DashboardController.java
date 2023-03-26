5a56e84b006234c7517deeace258a6114fe3cdb2
Sousa-Edson <edson3711.es@gmail.com>
2023-03-26 02:49:04 -0300

    criação de dashboard

--- src/main/java/com/belval/adocaoanimais/controller/DashboardController.java
+++ src/main/java/com/belval/adocaoanimais/controller/DashboardController.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 import com.belval.adocaoanimais.auxiliar.Menu;
 import com.belval.adocaoanimais.model.Adotar;
 import com.belval.adocaoanimais.model.Animal;
+import com.belval.adocaoanimais.model.Usuario;
 import com.belval.adocaoanimais.repository.AdotarRepository;
 import com.belval.adocaoanimais.repository.AnimalRepository;
 import com.belval.adocaoanimais.repository.UsuarioRepository;
@@ -17,42 +18,45 @@ import com.belval.adocaoanimais.repository.UsuarioRepository;
 @Controller
 public class DashboardController {
 
-    @Autowired
-    private UsuarioRepository usuarioRepository;
-    @Autowired
-    private AnimalRepository animalRepository;
-    @Autowired
-    private AdotarRepository adotarRepository;
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	@Autowired
+	private AnimalRepository animalRepository;
+	@Autowired
+	private AdotarRepository adotarRepository;
 
-    Menu menu = new Menu();
+	Menu menu = new Menu();
 
-    @GetMapping("/pet/dashboard")
-    public String novo() {
-        return "dashboard";
-    }
+	@GetMapping("/pet/dashboard")
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("dashboard");
+		List<Usuario> usuarios = this.usuarioRepository.findAll();
+		mv.addObject("usuario", usuarios);
+		return mv;
+	}
 
-    /* ANIMAL */
-    @GetMapping("/pet/admin/animal")
-    public ModelAndView indexAnimalAll() {
-        menu.setTitulo("Todos os anúncios de animais");
-        menu.setSelecao("anuncioAll");
-        List<Animal> animais = this.animalRepository.findAll();
-        ModelAndView mv = new ModelAndView("animal/index");
-        mv.addObject("animais", animais);
-        mv.addObject("menu", menu);
-        return mv;
-    }
+	/* ANIMAL */
+	@GetMapping("/pet/admin/animal")
+	public ModelAndView indexAnimalAll() {
+		menu.setTitulo("Todos os anúncios de animais");
+		menu.setSelecao("anuncioAll");
+		List<Animal> animais = this.animalRepository.findAll();
+		ModelAndView mv = new ModelAndView("animal/index");
+		mv.addObject("animais", animais);
+		mv.addObject("menu", menu);
+		return mv;
+	}
 
-    /* INTENÇÃO DE ADOTAR */
-    @GetMapping("/pet/admin/intencao-adotar")
-    public ModelAndView index() {
-        menu.setTitulo("Todas as solicitações de adotação");
-        menu.setSelecao("intencaoAll");
-        List<Adotar> animais = this.adotarRepository.findAll();
-        ModelAndView mv = new ModelAndView("intencao/index");
-        mv.addObject("animais", animais);
-        mv.addObject("menu", menu);
-        return mv;
-    }
+	/* INTENÇÃO DE ADOTAR */
+	@GetMapping("/pet/admin/intencao-adotar")
+	public ModelAndView index() {
+		menu.setTitulo("Todas as solicitações de adotação");
+		menu.setSelecao("intencaoAll");
+		List<Adotar> animais = this.adotarRepository.findAll();
+		ModelAndView mv = new ModelAndView("intencao/index");
+		mv.addObject("animais", animais);
+		mv.addObject("menu", menu);
+		return mv;
+	}
 
 }
